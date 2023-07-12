699fe749319da1518060c6e93c9c73b71411abc2
luizzotv <luiz.camisa10hjjyy@gmail.com>
2023-07-12 20:03:33 -0300

    aprendendo git

--- forca.py
+++ forca.py
@@ -1,3 +1,4 @@
+import random
 
 def jogar0():
 
@@ -5,36 +6,50 @@ def jogar0():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = 'banana'.upper()
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras [numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
     erros = 0
 
+    print(letras_acertadas)
+
     while(not enforcou and not acertou):
         
-        chute = input('Qual a letra? ')
+        chute = input("Qual a letra? ")
         chute = chute.strip().upper()
 
         if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
                 if (chute == letra):
-                    letras_acertadas [index] = letra 
+                    letras_acertadas[index] = letra 
                 index = index + 1
         else: 
             erros += 1
 
         enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+    
+    if (acertou):
+        print('Voce ganhou!')
+    else:
+        print('Voce perdeu!')
 
-    print('*******************************')
     print('FIM DE JOGO!')
     print('*******************************')
 if __name__ == "__main__":
     jogar0()
-    
-
-numeros = [10, 20, 30, 40, 50]
-
