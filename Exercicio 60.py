bd75dfb1bcae23fe1bcaf83d01cf728af6b459e5
luizzotv <luiz.camisa10hjjyy@gmail.com>
2023-07-16 14:28:19 -0300

    aprendendo git

--- Exercicio 60.py
+++ Exercicio 60.py
@@ -1,16 +1,11 @@
-#dias = int(input('Quantos dias: '))
-#horas = float(input('Quantas horas: '))
-#min = int(input('quantos minutos: '))
-#seg = float(input('quantos segundos: '))
-#somadias = dias * 86400
-#somahoras = horas * 3600
-#somamin = min * 60
-#print(f'a soma de {dias} dias {horas} horas, {min} min e {seg} segundos, somados todos em segundos é de: {somadias + somahoras + somamin + seg}s')###
 
-
-
-quantcigarros = int(input('Quantos cigarros voce fuma por dia? '))
-anosfumado = int(input('Por quantos anos voce ja fumou? '))
-perca = quantcigarros * 365 * anosfumado
-calculovida = perca / 144
-print(f'Se voce fumar {quantcigarros} cigarros por dia durante {anosfumado} anos voce perderá {calculovida:2f} dias de vida')
+n = int(input('Digite um numero para calcular seu fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' X ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
